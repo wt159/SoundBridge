895104e3008c3883886ec58f2288b53111a7c12e
wtp <15908282659@163.com>
2023-04-16 23:53:49 +0800

    添加boost编译脚本，加入cmake编译

---
+++ sdk/3rdparty/boost_1_81_0/windows_build.bat
@@ -0,0 +1,72 @@
+@echo off
+setlocal
+
+set system_name=%1
+set system_process=%2
+set toolchain=%3
+set prefix=../dist/%system_name%_%system_process%_%toolchain%
+set build_dir=../build
+set boost_dir=%cd%\..\boost_1_81_0
+
+echo boost_build: system_name is %system_name%
+echo boost_build: system_process is %system_process%
+echo boost_build: toolchain is %toolchain%
+echo boost_build: prefix is %prefix%
+echo boost_build: build_dir is %build_dir%
+echo boost_build: boost_dir is %boost_dir%
+echo boost_build: cd boost_dir
+cd %boost_dir%
+
+if exist %build_dir% (
+    echo build directory already exists
+) else (
+    mkdir %build_dir%
+)
+
+if exist %prefix% (
+    echo dist directory already exists
+) else (
+    mkdir %prefix%
+)
+
+if exist %boost_dir%\b2.exe (
+    echo boost_build: b2 already exists
+) else (
+    echo boost_build: b2 does not exist
+    echo boost_build: bootstrap
+    call bootstrap.bat %toolchain%
+)
+
+echo boost_build: b2
+@REM --build-type=complete variant=release threading=multi link=shared runtime-link=shared
+.\b2.exe -j 12 ^
+    --build-dir=%build_dir% --prefix=%prefix% ^
+    --without-atomic ^
+    --without-chrono ^
+    --without-container ^
+    --without-contract ^
+    --without-coroutine ^
+    --without-date_time ^
+    --without-exception ^
+    --without-fiber ^
+    --without-graph ^
+    --without-graph_parallel ^
+    --without-headers ^
+    --without-iostreams ^
+    --without-locale ^
+    --without-math ^
+    --without-nowide ^
+    --without-program_options ^
+    --without-python ^
+    --without-random ^
+    --without-regex ^
+    --without-stacktrace ^
+    --without-test ^
+    --without-thread ^
+    --without-timer ^
+    --without-type_erasure ^
+    --without-url ^
+    --without-wave ^
+    toolset=%toolchain% install
+
+endlocal
