41bc4659d3e012d37292fbaa1c56728ff620e0b1
wt159 <15908282659@163.com>
2023-09-17 23:01:16 +0800

    完善线程池代码

--- sdk/cosmos/ThreadPool.hpp
+++ sdk/cosmos/ThreadPool.hpp
@@ -1,83 +1,262 @@
 #pragma once
+#include "NonCopyable.hpp"
 #include "SyncQueue.hpp"
 #include <atomic>
+#include <chrono>
 #include <functional>
 #include <list>
 #include <memory>
 #include <thread>
 
-const int MaxTaskCount = 100;
-class ThreadPool {
+class ThreadPool : public NonCopyable {
 public:
-    using Task = std::function<void()>;
-    ThreadPool(int numThreads = std::thread::hardware_concurrency())
-        : m_queue(MaxTaskCount)
-    {
-        Start(numThreads);
-    }
+    enum class ThreadState { kInit = 0, kWaiting, kRunning, kStop };
+    enum class ThreadFlag { kInit = 0, kCore, kCache };
+    using Task              = std::function<void()>;
+    using PoolSeconds       = std::chrono::seconds;
+    using ThreadPtr         = std::shared_ptr<std::thread>;
+    using ThreadID          = std::atomic<size_t>;
+    using ThreadStateAtomic = std::atomic<ThreadState>;
+    using ThreadFlagAtomic  = std::atomic<ThreadFlag>;
+    using ThreadPoolLock    = std::unique_lock<std::mutex>;
+    struct ThreadPoolConfig {
+        size_t coreThreads       = std::thread::hardware_concurrency();
+        size_t maxThreads        = std::thread::hardware_concurrency();
+        size_t maxTaskSize       = 100;
+        PoolSeconds cacheTimeout = PoolSeconds(2);
 
-    ~ThreadPool(void)
-    {
-        //如果没有停止时则主动停止线程池
-        Stop();
-    }
+        ThreadPoolConfig() = default;
 
-    void Stop()
-    {
-        std::call_once(m_flag, [this] { StopThreadGroup(); }); //保证多线程情况下只调用一次StopThreadGroup
-    }
-
-    void AddTask(Task&& task)
-    {
-        m_queue.Put(std::forward<Task>(task));
-    }
-
-    void AddTask(const Task& task)
-    {
-        m_queue.Put(task);
-    }
-
-private:
-    void Start(int numThreads)
-    {
-        m_running = true;
-        //创建线程组
-        for (int i = 0; i < numThreads; ++i) {
-            m_threadgroup.push_back(std::make_shared<std::thread>(&ThreadPool::RunInThread, this));
+        ThreadPoolConfig(size_t coreThreadNum, size_t maxThreadNum, PoolSeconds cacheTimeoutSec)
+            : coreThreads(coreThreadNum)
+            , maxThreads(maxThreadNum)
+            , cacheTimeout(cacheTimeoutSec)
+        {
         }
+    };
+    struct ThreadWrapper {
+        ThreadPtr ptr;
+        ThreadID id;
+        ThreadStateAtomic state;
+        ThreadFlagAtomic flag;
+        ThreadWrapper()
+            : ptr(nullptr)
+            , id(0)
+            , state(ThreadState::kInit)
+            , flag(ThreadFlag::kInit)
+        {
+        }
+    };
+    using ThreadWrapperPtr = std::shared_ptr<ThreadWrapper>;
+
+public:
+    ThreadPool()
+        : m_cfg()
+        , m_queue(m_cfg.maxTaskSize)
+    {
+        init();
     }
 
-    void RunInThread()
+    ThreadPool(size_t coreThreadNum, size_t maxThreadNum, size_t cacheTimeoutSec)
+        : m_cfg(coreThreadNum, maxThreadNum, PoolSeconds(cacheTimeoutSec))
+        , m_queue(m_cfg.maxTaskSize)
     {
-        while (m_running) {
-            //取任务分别执行
-            std::list<Task> list;
-            m_queue.Take(list);
+    }
 
-            for (auto& task : list) {
-                if (!m_running)
-                    return;
+    ThreadPool(ThreadPoolConfig cfg)
+        : m_cfg(cfg)
+        , m_queue(m_cfg.maxTaskSize)
+    {
+        init();
+    }
 
-                task();
+    ~ThreadPool() { stop(); }
+
+    bool start()
+    {
+        if (!m_available.load()) {
+            return false;
+        }
+        std::call_once(m_flag, [this] { startThread(); });
+
+        return true;
+    }
+
+    bool stop(bool isShutdownNow = true)
+    {
+        if (!m_available.load()) {
+            return false;
+        }
+        std::call_once(m_flag, [this, isShutdownNow] { shutdown(isShutdownNow); });
+        return true;
+    }
+
+    template <typename F, typename... ARG> bool runTask(F f, ARG ...args)
+    {
+        if (!m_available.load() || !m_running.load() || m_shutdown.load()
+            || m_shutdownNow.load()) {
+            return false;
+        }
+
+        Task task = std::bind(f, args...);
+
+        m_queue.Put(task);
+        m_coreThreadCond.notify_one();
+        return true;
+    }
+
+    template <typename T, typename F, typename... ARG>
+    bool runTaskWithObj(T *obj, F f, ARG ...args)
+    {
+        if (!m_available.load() || !m_running.load() || m_shutdown.load()
+            || m_shutdownNow.load()) {
+            return false;
+        }
+
+        Task task = std::bind(std::mem_fn(f), obj, args...);
+
+        m_queue.Put(task);
+        m_coreThreadCond.notify_one();
+        return true;
+    }
+
+protected:
+    void threadloop(ThreadWrapperPtr threadPtr)
+    {
+        while (m_running.load()) {
+            if (m_shutdownNow.load()) {
+                break;
+            }
+            if (threadPtr->state.load() != ThreadState::kRunning) {
+                break;
+            }
+
+            size_t taskSize = m_queue.Size();
+            if (taskSize == 0) {
+                m_waitingThreadNum++;
+                if (threadPtr->flag.load() == ThreadFlag::kCore) {
+                    ThreadPoolLock lock(m_coreThreadMutex);
+                    m_coreThreadCond.wait(lock);
+                } else if (threadPtr->flag.load() == ThreadFlag::kCache) {
+                    ThreadPoolLock lock(m_cacheThreadMutex);
+                    auto state = m_cacheThreadCond.wait_for(lock, m_cfg.cacheTimeout);
+                    if (state == std::cv_status::timeout) {
+                        threadPtr->state.store(ThreadState::kStop);
+                        m_waitingThreadNum--;
+                        break;
+                    }
+                }
+            }
+
+            m_waitingThreadNum--;
+            taskSize = m_queue.Size();
+            if (m_waitingThreadNum.load() == 0 && taskSize > m_cfg.coreThreads) {
+                addCacheThread();
+            }
+
+            Task task;
+            m_queue.Take(task);
+
+            task();
+        }
+        return;
+    }
+    bool isValidConfig(const ThreadPoolConfig &cfg)
+    {
+        return !(cfg.coreThreads < 1 || cfg.coreThreads < cfg.maxThreads
+            || cfg.cacheTimeout < PoolSeconds(1));
+    }
+    void addCoreThread()
+    {
+        ThreadWrapperPtr ptr = std::make_shared<ThreadWrapper>();
+        m_coreThreadGroup.emplace_back(ptr);
+        ptr->id.store((size_t)ptr.get());
+        ptr->state.store(ThreadState::kRunning);
+        ptr->flag.store(ThreadFlag::kCore);
+        ThreadPtr thd(new std::thread(&ThreadPool::threadloop, this, ptr));
+        ptr->ptr = thd;
+    }
+    void addCacheThread()
+    {
+        auto iter = m_cacheThreadGroup.begin();
+        for (; iter != m_cacheThreadGroup.end();) {
+            if ((*iter)->state.load() == ThreadState::kStop) {
+                if ((*iter)->ptr->joinable()) {
+                    (*iter)->ptr->join();
+                }
+                m_cacheThreadGroup.erase(iter);
+            } else {
+                iter++;
             }
         }
-    }
 
-    void StopThreadGroup()
+        ThreadWrapperPtr ptr = std::make_shared<ThreadWrapper>();
+        m_cacheThreadGroup.emplace_back(ptr);
+        ptr->id.store((size_t)ptr.get());
+        ptr->state.store(ThreadState::kRunning);
+        ptr->flag.store(ThreadFlag::kCore);
+        ThreadPtr thd(new std::thread(&ThreadPool::threadloop, this, ptr));
+        ptr->ptr = thd;
+    }
+    void init()
     {
-        m_queue.Stop(); //让同步队列中的线程停止
-        m_running = false; //置为false，让内部线程跳出循环并退出
+        m_totalTaskNum.store(0);
+        m_waitingThreadNum.store(0);
+        m_shutdown.store(false);
+        m_shutdownNow.store(false);
+        m_running.store(false);
+        m_coreThreadGroup.clear();
+        m_cacheThreadGroup.clear();
 
-        for (auto thread : m_threadgroup) //等待线程结束
-        {
-            if (thread)
-                thread->join();
+        if (!isValidConfig(m_cfg)) {
+            m_available.store(false);
+        } else {
+            m_available.store(true);
         }
-        m_threadgroup.clear();
+    }
+    void startThread()
+    {
+        for (size_t i = 0; i < m_cfg.coreThreads; ++i) {
+            addCoreThread();
+        }
+        for (size_t i = 0; i < m_cfg.maxThreads - m_cfg.coreThreads; ++i) {
+            addCacheThread();
+        }
+        m_running.store(true);
+    }
+    void shutdown(bool isShutdownNow)
+    {
+        m_shutdown.store(!isShutdownNow);
+        m_shutdownNow.store(isShutdownNow);
+        for (auto &temp : m_coreThreadGroup) {
+            if (temp->ptr->joinable()) {
+                temp->ptr->join();
+            }
+        }
+        for (auto &temp : m_cacheThreadGroup) {
+            if (temp->ptr->joinable()) {
+                temp->ptr->join();
+            }
+        }
+
+        m_coreThreadGroup.clear();
+        m_cacheThreadGroup.clear();
+        m_running.store(false);
     }
 
-    std::list<std::shared_ptr<std::thread>> m_threadgroup; //处理任务的线程组
-    SyncQueue<Task> m_queue; //同步队列
-    atomic_bool m_running; //是否停止的标志
+    ThreadPoolConfig m_cfg;
+    std::list<ThreadWrapperPtr> m_coreThreadGroup;
+    std::mutex m_coreThreadMutex;
+    std::condition_variable m_coreThreadCond;
+    std::list<ThreadWrapperPtr> m_cacheThreadGroup; // 空闲线程组
+    std::mutex m_cacheThreadMutex;
+    std::condition_variable m_cacheThreadCond;
+    SyncQueue<Task> m_queue;
+    std::atomic<size_t> m_totalTaskNum;
+    std::atomic<size_t> m_waitingThreadNum;
+    std::atomic<bool> m_running;
+    std::atomic<bool> m_available;
+    std::atomic<bool> m_shutdown;
+    std::atomic<bool> m_shutdownNow;
     std::once_flag m_flag;
 };
