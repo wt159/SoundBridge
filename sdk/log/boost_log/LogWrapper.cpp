ca5a6bcbd159f2ea44bff2b5760643e0661acdd9
wt159 <15908282659@163.com>
2026-03-08 17:13:41 +0800

    build: switch to Boost.Log backend and fix Windows build

--- sdk/log/boost_log/LogWrapper.cpp
+++ sdk/log/boost_log/LogWrapper.cpp
@@ -1,17 +1,20 @@
 #include "LogWrapper.h"
-#include <stdexcept>
-#include <string>
-#include <iostream>
-#include <thread>
-#include <boost/smart_ptr/shared_ptr.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 
-#include <boost/log/common.hpp>
-#include <boost/log/expressions.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/log/attributes.hpp>
-#include <boost/log/sources/logger.hpp>
+#include <boost/log/common.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
 #include <boost/log/sinks/sync_frontend.hpp>
 #include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/make_shared.hpp>
+
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
 
 namespace logging = boost::log;
 namespace attrs = boost::log::attributes;
@@ -20,10 +23,7 @@ namespace sinks = boost::log::sinks;
 namespace expr = boost::log::expressions;
 namespace keywords = boost::log::keywords;
 
-using boost::shared_ptr;
-
-enum severity_level
-{
+enum severity_level {
     normal,
     warning,
     error,
@@ -32,100 +32,27 @@ enum severity_level
     verbose
 };
 
-// The formatting logic for the severity level
-template< typename CharT, typename TraitsT >
-inline std::basic_ostream< CharT, TraitsT >& operator<< (std::basic_ostream< CharT, TraitsT >& strm, severity_level lvl)
+template <typename CharT, typename TraitsT>
+inline std::basic_ostream<CharT, TraitsT>& operator<<(std::basic_ostream<CharT, TraitsT>& strm,
+                                                      severity_level lvl)
 {
-    static const char* const str[] =
-    {
-        "I",
-        "W",
-        "E",
-        "F",
-        "D",
-        "V"
-    };
-    if (static_cast< std::size_t >(lvl) < (sizeof(str) / sizeof(*str)))
+    static const char* const str[] = {"I", "W", "E", "F", "D", "V"};
+    if (static_cast<std::size_t>(lvl) < (sizeof(str) / sizeof(*str))) {
         strm << str[lvl];
-    else
-        strm << static_cast< int >(lvl);
+    } else {
+        strm << static_cast<int>(lvl);
+    }
     return strm;
 }
 
-void thread_func(int i)
-{
-    src::logger lg;
-    BOOST_LOG(lg) << i << " Some log record";
-}
-
-class attrs_log_pid:
-    public logging::v2_mt_posix::attribute
-{
-public:
-    using value_type = uint32_t;
-protected:
-    static value_type current_pid() {
-        return (value_type)getpid();
-    }
-
-protected:
-    //! Attribute factory implementation
-    struct BOOST_SYMBOL_VISIBLE impl :
-        public logging::v2_mt_posix::attribute::impl
-    {
-        logging::v2_mt_posix::attribute_value get_value()
-        {
-            typedef logging::v2_mt_posix::attributes::attribute_value_impl< value_type > result_value;
-            return logging::v2_mt_posix::attribute_value(new result_value(current_pid()));
-        }
-    };
-
-public:
-    attrs_log_pid() : logging::v2_mt_posix::attribute(new impl()) {}
-    ~attrs_log_pid() = default;
-};
-
-class attrs_log_tid:
-    public logging::v2_mt_posix::attribute
-{
-public:
-    using value_type = uint32_t;
-protected:
-    static value_type current_tid() {
-        return (value_type)syscall(SYS_gettid);
-    }
-
-protected:
-    //! Attribute factory implementation
-    struct BOOST_SYMBOL_VISIBLE impl :
-        public logging::v2_mt_posix::attribute::impl
-    {
-        logging::v2_mt_posix::attribute_value get_value()
-        {
-            typedef logging::v2_mt_posix::attributes::attribute_value_impl< value_type > result_value;
-            return logging::v2_mt_posix::attribute_value(new result_value(current_tid()));
-        }
-    };
-
-public:
-    attrs_log_tid() : logging::v2_mt_posix::attribute(new impl()) {}
-    ~attrs_log_tid() = default;
-};
-
 std::unique_ptr<LogWrapper> LogWrapper::m_staticLog;
 
-class LogWrapper::Impl
-{
+class LogWrapper::Impl {
 private:
-    typedef sinks::synchronous_sink< sinks::text_file_backend > file_sink;
-    boost::shared_ptr< file_sink > m_sink;
-    src::severity_logger< severity_level > m_slg;
-    std::string m_logDir;
-    std::string m_logFileName;
-    int m_singleFileSizeInBytes;
-    int m_maxFileCount;
+    using file_sink = sinks::synchronous_sink<sinks::text_file_backend>;
+
 public:
-    Impl(std::string & logDir, std::string & logFileName, int singleFileSizeInBytes, int maxFileCount)
+    Impl(std::string& logDir, std::string& logFileName, int singleFileSizeInBytes, int maxFileCount)
         : m_sink(nullptr)
         , m_slg()
         , m_logDir(logDir)
@@ -135,20 +62,25 @@ public:
     {
         init();
     }
-    ~Impl() {
-        m_sink->flush();
+
+    ~Impl()
+    {
+        if (m_sink) {
+            m_sink->flush();
+        }
     }
-    void write(int level, const char *tag, const char *format, va_list args)
+
+    void write(int level, const char* tag, const char* format, va_list args)
     {
         char buf[1024] = {0};
-        int tagLen = strlen(tag);
+        const int tagLen = static_cast<int>(strlen(tag));
         strncpy(buf, tag, tagLen);
         buf[tagLen] = ' ';
         buf[tagLen + 1] = ':';
         buf[tagLen + 2] = ' ';
-        vsnprintf(buf + tagLen +3, 1024 - tagLen - 3, format, args);
-        switch (level)
-        {
+        vsnprintf(buf + tagLen + 3, 1024 - tagLen - 3, format, args);
+
+        switch (level) {
         case logLevel::INFO:
             BOOST_LOG_SEV(m_slg, normal) << buf;
             printf("INFO    %s\n", buf);
@@ -175,75 +107,76 @@ public:
             break;
         default:
             break;
+        }
     }
-    }
-protected:
-    void init() {
-        // Create a text file sink
-        std::string file_name = m_logDir + "/";
-        file_name += "%Y%m%d_%H%M%S_%5N.log";
-        typedef sinks::synchronous_sink< sinks::text_file_backend > file_sink;
-        boost::shared_ptr< file_sink > sink(new file_sink(
-            keywords::file_name = file_name.c_str(),                       // file name pattern
-            keywords::target_file_name = "%Y%m%d_%H%M%S_%5N.log",   // file name pattern
-            keywords::rotation_size = m_singleFileSizeInBytes       // rotation size, in characters
-            ));
 
-        // Set up where the rotated files will be stored
+private:
+    void init()
+    {
+        std::string fileNamePattern = m_logDir + "/%Y%m%d_%H%M%S_%5N.log";
+
+        auto sink = boost::make_shared<file_sink>(
+            keywords::file_name = fileNamePattern.c_str(),
+            keywords::target_file_name = "%Y%m%d_%H%M%S_%5N.log",
+            keywords::rotation_size = m_singleFileSizeInBytes);
+
         sink->locked_backend()->set_file_collector(sinks::file::make_collector(
-            keywords::target = "logs",                              // where to store rotated files
-            keywords::max_size = m_singleFileSizeInBytes,                  // maximum total size of the stored files, in bytes
-            // keywords::min_free_space = 100 * 1024 * 1024,           // minimum free space on the drive, in bytes
-            keywords::max_files = m_maxFileCount                               // maximum number of stored files
-            ));
+            keywords::target = m_logDir,
+            keywords::max_size = static_cast<uintmax_t>(m_singleFileSizeInBytes)
+                * static_cast<uintmax_t>(m_maxFileCount),
+            keywords::max_files = static_cast<uintmax_t>(m_maxFileCount)));
 
-        // Upon restart, scan the target directory for files matching the file_name pattern
         sink->locked_backend()->scan_for_files();
 
-        sink->set_formatter
-        (
-            expr::format("[%1%] [%2%] [%3%] [%4%] - %5%")
-                % expr::attr< boost::posix_time::ptime >("TimeStamp")
-                % expr::attr< attrs_log_tid::value_type >("TID")
-                % expr::attr< attrs_log_pid::value_type >("PID")
-                % expr::attr< severity_level >("Severity")
-                % expr::smessage
-        );
+        sink->set_formatter(expr::stream
+                            << "["
+                            << expr::format_date_time<boost::posix_time::ptime>(
+                                   "TimeStamp", "%Y-%m-%d %H:%M:%S")
+                            << "] [" << expr::attr<attrs::current_thread_id::value_type>("ThreadID")
+                            << "] [" << expr::attr<severity_level>("Severity")
+                            << "] - " << expr::smessage);
 
-        // Add it to the core
         logging::core::get()->add_sink(sink);
-
-        // Add some attributes too
-        logging::core::get()->add_global_attribute("TimeStamp", attrs::local_clock());
-        logging::core::get()->add_global_attribute("TID", attrs_log_tid());
-        logging::core::get()->add_global_attribute("PID", attrs_log_pid());
+        logging::add_common_attributes();
 
         m_sink = sink;
     }
+
+private:
+    boost::shared_ptr<file_sink> m_sink;
+    src::severity_logger<severity_level> m_slg;
+    std::string m_logDir;
+    std::string m_logFileName;
+    int m_singleFileSizeInBytes;
+    int m_maxFileCount;
 };
 
-void LogWrapper::initialize(std::string & logDir, std::string & logFileName, int singleFileSizeInBytes, int maxFileCount)
+void LogWrapper::initialize(std::string& logDir, std::string& logFileName, int singleFileSizeInBytes,
+                            int maxFileCount)
 {
-    if(m_staticLog)
+    if (m_staticLog) {
         return;
-    m_staticLog.reset(new LogWrapper);
-    m_staticLog->m_impl.reset(new LogWrapper::Impl(logDir, logFileName, singleFileSizeInBytes, maxFileCount));
+    }
+    m_staticLog.reset(new LogWrapper());
+    m_staticLog->m_impl.reset(
+        new LogWrapper::Impl(logDir, logFileName, singleFileSizeInBytes, maxFileCount));
 }
 
-void LogWrapper::getInstanceInitialize(std::string & logDir, std::string & logFileName, int singleFileSizeInBytes, int maxFileCount)
+void LogWrapper::getInstanceInitialize(std::string& logDir, std::string& logFileName,
+                                       int singleFileSizeInBytes, int maxFileCount)
 {
-    LogWrapper::initialize(logDir,logFileName, singleFileSizeInBytes, maxFileCount);
+    LogWrapper::initialize(logDir, logFileName, singleFileSizeInBytes, maxFileCount);
 }
 
-LogWrapper * LogWrapper::getInstance()
+LogWrapper* LogWrapper::getInstance()
 {
     return m_staticLog.get();
 }
 
-void LogWrapper::log(int level, const char * tag, const char * format, ...)
+void LogWrapper::log(int level, const char* tag, const char* format, ...)
 {
     va_list args;
-    va_start(args, format); 
+    va_start(args, format);
     m_impl->write(level, tag, format, args);
     va_end(args);
 }
